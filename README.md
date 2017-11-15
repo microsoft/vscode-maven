fb293a96cdab0220b434be3ce98197127387acc2
Yan Zhang <yanzh@microsoft.com>
2017-11-15 14:59:07 +0800

    add effective-pom

--- README.md
+++ README.md
@@ -5,10 +5,13 @@ This is the README for extension "vscode-maven".
 ## Features
 Maven extension for VS Code. It now reads `pom.xml` in root folder, and provide project structures in sidebar.
 
-1. multi-module projects supported.
-1. common goals can be executed via Right-Click, namely `clean`, `validate`, `compile`, `test`, `package`, `verify`, `install`, `site`, `deploy`.
+* multi-module projects supported.
+* common goals can be executed via Right-Click, namely `clean`, `validate`, `compile`, `test`, `package`, `verify`, `install`, `site`, `deploy`.
 
 ![Screenshot](images/Capture.PNG)
+
+* support generating effective pom.
+![Screenshot](images/Capture2.PNG)
 
 ## Requirements
 
@@ -17,3 +20,7 @@ Maven installed and PATH added, i.e., `mvn` command can be executed directly in 
 ## Release Notes
 
 ### 0.0.1 Projects listed in sidebar.
+
+### 0.0.2 Add icons.
+
+### 0.0.3 Support generating effective-pom.
--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from "path";
+import * as md5 from "md5";
 import { MavenProjectsTreeDataProvider } from './mavenProjectsTreeDataProvider';
 import { Utils } from './utils';
 import { MavenProjectTreeItem } from './mavenProjectTreeItem';
+import { execSync, exec, spawn } from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,19 +16,52 @@ export function activate(context: vscode.ExtensionContext) {
 
     const mavenProjectsTreeDataProvider = new MavenProjectsTreeDataProvider(context);
     vscode.window.registerTreeDataProvider("mavenProjects", mavenProjectsTreeDataProvider);
-    
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let commandMavenProjectsRefresh = vscode.commands.registerCommand('mavenProjects.refresh', () => {
         mavenProjectsTreeDataProvider.refreshTree();
     });
-    let commandMavenGoalExecute= vscode.commands.registerCommand('mavenGoal.exec', (goalItem) => {
+    let commandMavenGoalExecute = vscode.commands.registerCommand('mavenGoal.exec', (goalItem) => {
         const item = goalItem as MavenProjectTreeItem;
         Utils.runInTerminal(`mvn ${item.label} -f "${item.pomXmlFilePath}"`);
     });
+   
+    let commandMavenProjectEffectivePom = vscode.commands.registerCommand('mavenProject.effectivePom', (goalItem) => {
+        const item = goalItem as MavenProjectTreeItem;
+        const tmpdir = process.env["TEMP"];
+        const filepath = path.join(tmpdir, md5(item.pomXmlFilePath), 'effective-pom.xml');
+        let p = new Promise((resolve, reject) => {
+            exec(`mvn help:effective-pom -f "${item.pomXmlFilePath}" -Doutput="${filepath}"`, (error, stdout, stderr) => {
+                if (error || stderr) {
+                    console.error(error);
+                    console.error(stderr);
+                    return resolve(false);
+                }
+                resolve(true);
+            });
+        }).then(ret => {
+            if (ret) {
+                vscode.window.showTextDocument(vscode.Uri.file(filepath));
+            } else {
+                vscode.window.showErrorMessage("Error occurred in generating effective pom.");
+            }
+        })
+        vscode.window.setStatusBarMessage("Generating effective pom ... ", p);
+    });
+
+    ['clean', 'validate', 'compile', 'test', 'package', 'verify', 'install', 'site', 'deploy'].forEach(goal => {
+        let commandMavenGoal = vscode.commands.registerCommand(`mavenGoal.${goal}`, (goalItem) => {
+            const item = goalItem as MavenProjectTreeItem;
+            Utils.runInTerminal(`mvn ${goal} -f "${item.pomXmlFilePath}"`);
+        });
+        context.subscriptions.push(commandMavenGoal);
+    });
 
     context.subscriptions.push(commandMavenProjectsRefresh);
+    context.subscriptions.push(commandMavenGoalExecute);
+    context.subscriptions.push(commandMavenProjectEffectivePom);
 }
 
 // this method is called when your extension is deactivated
