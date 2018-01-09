ca5bae5bc41472309bd831e2a5768ad214b55f06
Yan Zhang <yanzh@microsoft.com>
2018-01-09 11:40:30 +0800

    update telemetry

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 "use strict";
 import * as vscode from "vscode";
 import { Progress, Uri } from "vscode";
-import { TelemetryWrapper, Transaction } from "vscode-extension-telemetry-wrapper";
+import { TelemetryWrapper } from "vscode-extension-telemetry-wrapper";
 import { ArchetypeModule } from "./ArchetypeModule";
 import { ProjectItem } from "./model/ProjectItem";
 import { ProjectDataProvider } from "./ProjectDataProvider";
@@ -12,7 +12,7 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     await Utils.loadPackageInfo(context);
     // Usage data statistics.
     if (Utils.getAiKey()) {
-        TelemetryWrapper.setEnabled(vscode.workspace.getConfiguration("maven").get<boolean>("enableStatistics"));
+        TelemetryWrapper.setEnabled(() => vscode.workspace.getConfiguration("maven").get<boolean>("enableStatistics"));
         TelemetryWrapper.initilize(Utils.getExtensionPublisher(), Utils.getExtensionName(), Utils.getExtensionVersion(), Utils.getAiKey());
     }
 
@@ -20,35 +20,33 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
     vscode.window.registerTreeDataProvider("mavenProjects", mavenProjectsTreeDataProvider);
 
     ["clean", "validate", "compile", "test", "package", "verify", "install", "site", "deploy"].forEach((goal: string) => {
-        TelemetryWrapper.registerCommand(context, `maven.goal.${goal}`, async (_t: Transaction, item: ProjectItem) => {
-            await mavenProjectsTreeDataProvider.executeGoal(item, goal);
+        TelemetryWrapper.registerCommand(context, `maven.goal.${goal}`, () => {
+            return (item: ProjectItem): Promise<void> => mavenProjectsTreeDataProvider.executeGoal(item, goal);
         });
     });
 
-    TelemetryWrapper.registerCommand(context, "maven.project.refreshAll", async (_t: Transaction) => {
-        await mavenProjectsTreeDataProvider.refreshTree();
+    TelemetryWrapper.registerCommand(context, "maven.project.refreshAll", () => {
+        return (): void => mavenProjectsTreeDataProvider.refreshTree();
     });
 
-    TelemetryWrapper.registerCommand(context, "maven.project.effectivePom", async (_t: Transaction, item: Uri | ProjectItem) => {
-        await mavenProjectsTreeDataProvider.effectivePom(item);
+    TelemetryWrapper.registerCommand(context, "maven.project.effectivePom", () => {
+        return (item: Uri | ProjectItem): Promise<void> => mavenProjectsTreeDataProvider.effectivePom(item);
     });
 
-    TelemetryWrapper.registerCommand(context, "maven.goal.custom", async (_t: Transaction, item: ProjectItem) => {
-        await mavenProjectsTreeDataProvider.customGoal(item);
+    TelemetryWrapper.registerCommand(context, "maven.goal.custom", () => {
+        return (item: ProjectItem): Promise<void> => mavenProjectsTreeDataProvider.customGoal(item);
     });
 
-    TelemetryWrapper.registerCommand(context, "maven.project.openPom", async (_t: Transaction, item: ProjectItem) => {
-        if (item) {
-            await VSCodeUI.openFileIfExists(item.abosolutePath);
-        }
+    TelemetryWrapper.registerCommand(context, "maven.project.openPom", () => {
+        return (item: ProjectItem): Promise<void> => item ? VSCodeUI.openFileIfExists(item.abosolutePath) : null;
     });
 
-    TelemetryWrapper.registerCommand(context, "maven.archetype.generate", async (_t: Transaction, entry: Uri | undefined) => {
-        await ArchetypeModule.generateFromArchetype(entry);
+    TelemetryWrapper.registerCommand(context, "maven.archetype.generate", () => {
+        return (entry: Uri | undefined): Promise<void> => ArchetypeModule.generateFromArchetype(entry);
     });
 
-    TelemetryWrapper.registerCommand(context, "maven.archetype.update", async (_t: Transaction) => {
-        await vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, async (p: Progress<{}>) => {
+    TelemetryWrapper.registerCommand(context, "maven.archetype.update", () => {
+        return (): Thenable<void> => vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, async (p: Progress<{}>) => {
             p.report({ message: "updating archetype catalog ..." });
             await ArchetypeModule.updateArchetypeCatalog();
             p.report({ message: "finished." });
