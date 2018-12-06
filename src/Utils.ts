53f916045b990c294966bf16550431ba15fd0ea7
Yan Zhang <yanzh@microsoft.com>
2018-12-06 10:06:17 +0800

    Show effective pom in the active ViewColumn.

--- src/Utils.ts
+++ src/Utils.ts
@@ -9,7 +9,7 @@ import * as md5 from "md5";
 import * as os from "os";
 import * as path from "path";
 import * as url from "url";
-import { commands, ExtensionContext, extensions, Progress, ProgressLocation, RelativePattern, TextDocument, Uri, window, workspace, WorkspaceFolder } from 'vscode';
+import { commands, ExtensionContext, extensions, Progress, ProgressLocation, RelativePattern, TextDocument, Uri, window, workspace, WorkspaceFolder, ViewColumn } from 'vscode';
 import { setUserError } from "vscode-extension-telemetry-wrapper";
 import * as xml2js from "xml2js";
 import { mavenExplorerProvider } from "./explorer/MavenExplorerProvider";
@@ -324,7 +324,7 @@ export namespace Utils {
 
         if (pomxml) {
             const document: TextDocument = await workspace.openTextDocument({ language: "xml", content: pomxml });
-            window.showTextDocument(document);
+            window.showTextDocument(document, ViewColumn.Active);
         }
     }
 
