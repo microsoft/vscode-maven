10ef283dc782b72d83a1b60d1d9b68e6e2f4ec4f
yanzh <yanzh@microsoft.com>
2018-12-08 15:35:49 +0800

    Update icons for tree items.

--- src/explorer/model/PluginGoal.ts
+++ src/explorer/model/PluginGoal.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 import * as vscode from "vscode";
+import { Utils } from "../../Utils";
 import { ITreeItem } from "./ITreeItem";
 import { MavenPlugin } from "./MavenPlugin";
 
@@ -18,6 +19,11 @@ export class PluginGoal implements ITreeItem {
         return "PluginGoal";
     }
     public getTreeItem(): vscode.TreeItem {
-        return new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.None);
+        const treeItem: vscode.TreeItem =  new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.None);
+        treeItem.iconPath = {
+            light: Utils.getResourcePath("light", "goal.svg"),
+            dark: Utils.getResourcePath("dark", "goal.svg")
+        };
+        return treeItem;
     }
 }
