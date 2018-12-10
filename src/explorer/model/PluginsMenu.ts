17e11f5cae82fed77abd01bb4659d238f0d43323
Yan Zhang <Eskibear@users.noreply.github.com>
2018-12-10 10:06:40 +0800

    Update icons for tree items. (#168)
    * Update icons for tree items.
    
    * Remove empty elements in svg.

--- src/explorer/model/PluginsMenu.ts
+++ src/explorer/model/PluginsMenu.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import * as vscode from "vscode";
+import { Utils } from "../../Utils";
 import { ITreeItem } from "./ITreeItem";
 import { MavenPlugin } from "./MavenPlugin";
 import { MavenProject } from "./MavenProject";
@@ -14,5 +16,14 @@ export class PluginsMenu extends Menu implements ITreeItem {
 
     public getChildren() : MavenPlugin[] {
         return this._project.plugins;
+    }
+
+    public getTreeItem(): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        const treeItem: vscode.TreeItem = new vscode.TreeItem(this._name, vscode.TreeItemCollapsibleState.Collapsed);
+        treeItem.iconPath = {
+            light: Utils.getResourcePath("light", "pluginMenu.svg"),
+            dark: Utils.getResourcePath("dark", "pluginMenu.svg")
+        };
+        return treeItem;
     }
 }
