f0e5e6c5779439fddbe319ce9819bc322a49cebb
Yan Zhang <Eskibear@users.noreply.github.com>
2018-12-17 12:47:04 +0800

    Update icons. (#181)

--- src/explorer/model/PluginGoal.ts
+++ src/explorer/model/PluginGoal.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import * as vscode from "vscode";
-import { Utils } from "../../Utils";
+// import { Utils } from "../../Utils";
 import { ITreeItem } from "./ITreeItem";
 import { MavenPlugin } from "./MavenPlugin";
 
@@ -19,11 +19,12 @@ export class PluginGoal implements ITreeItem {
         return "PluginGoal";
     }
     public getTreeItem(): vscode.TreeItem {
-        const treeItem: vscode.TreeItem =  new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.None);
-        treeItem.iconPath = {
-            light: Utils.getResourcePath("light", "goal.svg"),
-            dark: Utils.getResourcePath("dark", "goal.svg")
-        };
-        return treeItem;
+        return new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.None);
+        // const treeItem: vscode.TreeItem = new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.None);
+        // treeItem.iconPath = {
+        //     light: Utils.getResourcePath("light", "goal.svg"),
+        //     dark: Utils.getResourcePath("dark", "goal.svg")
+        // };
+        // return treeItem;
     }
 }
--- src/explorer/model/PluginsMenu.ts
+++ src/explorer/model/PluginsMenu.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT license.
 
 import * as vscode from "vscode";
-import { Utils } from "../../Utils";
+// import { Utils } from "../../Utils";
 import { ITreeItem } from "./ITreeItem";
 import { MavenPlugin } from "./MavenPlugin";
 import { MavenProject } from "./MavenProject";
@@ -20,12 +20,13 @@ export class PluginsMenu extends Menu implements ITreeItem {
     }
 
     public getTreeItem(): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        const treeItem: vscode.TreeItem = new vscode.TreeItem(this._name, vscode.TreeItemCollapsibleState.Collapsed);
-        treeItem.iconPath = {
-            light: Utils.getResourcePath("light", "pluginMenu.svg"),
-            dark: Utils.getResourcePath("dark", "pluginMenu.svg")
-        };
-        return treeItem;
+        return new vscode.TreeItem(this._name, vscode.TreeItemCollapsibleState.Collapsed);
+        // const treeItem: vscode.TreeItem = new vscode.TreeItem(this._name, vscode.TreeItemCollapsibleState.Collapsed);
+        // treeItem.iconPath = {
+        //     light: Utils.getResourcePath("light", "pluginMenu.svg"),
+        //     dark: Utils.getResourcePath("dark", "pluginMenu.svg")
+        // };
+        // return treeItem;
     }
 
     public async refresh(): Promise<void> {
