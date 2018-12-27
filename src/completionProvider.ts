017e29b145879ffde9ebf97ec92c3700dae953fc
Yan Zhang <yanzh@microsoft.com>
2018-12-27 09:57:00 +0800

    Add license header and remove comments.

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
 
 import * as vscode from "vscode";
 
@@ -5,11 +7,6 @@ class CompletionProvider implements vscode.CompletionItemProvider {
     public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList> {
         throw new Error("Method not implemented.");
     }
-
-    // public resolveCompletionItem?(item: vscode.CompletionItem, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CompletionItem> {
-    //     throw new Error("Method not implemented.");
-    // }
-
 }
 
 export const completionProvider: CompletionProvider = new CompletionProvider();
