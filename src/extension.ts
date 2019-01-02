13a87c80fc1fb2bdc5633843856baa618bb8230f
Yan Zhang <Eskibear@users.noreply.github.com>
2019-01-02 15:06:21 +0800

    Specify textEdit for completion items. (#205)

--- src/extension.ts
+++ src/extension.ts
@@ -141,7 +141,7 @@ async function doActivate(_operationId: string, context: vscode.ExtensionContext
     );
     // completion item provider
     if (vscode.workspace.getConfiguration("maven", null).get<boolean>("completion.enabled")) {
-        context.subscriptions.push(vscode.languages.registerCompletionItemProvider([{ language: "xml", scheme: "file", pattern: "**/pom.xml" }], completionProvider));
+        context.subscriptions.push(vscode.languages.registerCompletionItemProvider([{ language: "xml", scheme: "file", pattern: "**/pom.xml" }], completionProvider, "."));
         vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, (progress) => {
             progress.report({ message: "Updating local Maven repository indices" });
             return completionProvider.initialize();
