9a5eb3b3df64c73540ba3c4e6fc7e41e38baa08b
Yan Zhang <Eskibear@users.noreply.github.com>
2019-01-29 15:50:32 +0800

    sort for non-strict semantic versions (#237)

--- src/completion/versionUtils.ts
+++ src/completion/versionUtils.ts
@@ -2,13 +2,12 @@
 // Licensed under the MIT license.
 
 import * as _ from "lodash";
-import {parse, SemVer} from "semver";
 
 const VERSION_VALUE_MAX: number = 999;
 const VERSION_VALUE_DIGITS: number = 3;
 // tslint:disable-next-line:export-name
 export function getSortText(version: string): string {
-    const ver: SemVer = parse(version);
-    const versionValues: number[] = ver ? [ver.major, ver.minor, ver.patch] : [0, 0, 0];
-    return versionValues.map(v => _.padStart((VERSION_VALUE_MAX - v).toString(), VERSION_VALUE_DIGITS, "0")).join();
+    const segments: string[] = version.split(/\.|-/);
+    const [major, minor, patch] = segments.map(x => Number.parseInt(x, 10)).map(x => Number.isInteger(x) ? x : 0);
+    return [major, minor, patch].map(v => _.padStart((VERSION_VALUE_MAX - v).toString(), VERSION_VALUE_DIGITS, "0")).join("");
 }
