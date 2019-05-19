2b72eb735d062f38a80153950bc1454ff6599767
yanzh <yanzh@microsoft.com>
2019-05-19 22:22:22 +0800

    by default not re-calucate effective pom when changes detected

--- src/explorer/model/MavenProject.ts
+++ src/explorer/model/MavenProject.ts
@@ -120,10 +120,6 @@ export class MavenProject implements ITreeItem {
 
     public async refresh(): Promise<void> {
         await this._refreshPom();
-        this._effectivePom.update()
-        .then(() => {
-            mavenExplorerProvider.refresh(this);
-        }).catch(console.error); // no await to unblock the thread.
     }
 
     public async parsePom(): Promise<void> {
