2ec4590403d02636274c848813af18d22c8c3dc3
Yan Zhang <yanzh@microsoft.com>
2019-07-01 10:21:56 +0800

    fix unordered import

--- src/explorer/model/MavenProject.ts
+++ src/explorer/model/MavenProject.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import * as _ from "lodash";
 import * as fs from "fs";
+import * as _ from "lodash";
 import * as path from "path";
 import * as vscode from "vscode";
 import { Settings } from "../../Settings";
