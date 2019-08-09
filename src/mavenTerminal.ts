7981fa917c7745ff2bcf7450cc5749786618310e
Yan Zhang <Eskibear@users.noreply.github.com>
2019-08-10 00:07:36 +0800

    makes tslint happy

--- src/mavenTerminal.ts
+++ src/mavenTerminal.ts
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-import * as os from "os";
 import * as vscode from "vscode";
 import { mavenOutputChannel } from "./mavenOutputChannel";
 import { Settings } from "./Settings";
