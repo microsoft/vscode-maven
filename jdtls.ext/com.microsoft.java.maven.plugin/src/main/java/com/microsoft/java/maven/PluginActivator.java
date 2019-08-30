8ceb593db1e23b834adec5eb7fe2716cf4e3f931
Yan Zhang <yanzh@microsoft.com>
2019-08-30 14:15:03 +0800

    Use EPL license for .java code

--- jdtls.ext/com.microsoft.java.maven.plugin/src/main/java/com/microsoft/java/maven/PluginActivator.java
+++ jdtls.ext/com.microsoft.java.maven.plugin/src/main/java/com/microsoft/java/maven/PluginActivator.java
@@ -1,7 +1,15 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
+/*******************************************************************************
+ * Copyright (c) 2019 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
 
-package com.microsoft.java.maven;
+ package com.microsoft.java.maven;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
--- jdtls.ext/com.microsoft.java.maven.plugin/src/main/java/com/microsoft/java/maven/handler/DelegateCommandHandler.java
+++ jdtls.ext/com.microsoft.java.maven.plugin/src/main/java/com/microsoft/java/maven/handler/DelegateCommandHandler.java
@@ -1,5 +1,13 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
+/*******************************************************************************
+ * Copyright (c) 2019 Microsoft Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Microsoft Corporation - initial API and implementation
+ *******************************************************************************/
 
 package com.microsoft.java.maven.handler;
 
