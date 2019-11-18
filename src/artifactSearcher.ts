704356c6333f1dd533ac7754c6adaf26afae11ad
Yan Zhang <yanzh@microsoft.com>
2019-11-18 16:36:13 +0800

    enable lazy activation of JLS
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/artifactSearcher.ts
+++ src/artifactSearcher.ts
@@ -2,47 +2,156 @@
 // Licensed under the MIT license.
 
 import * as path from "path";
-import { CancellationToken, CodeAction, CodeActionContext, CodeActionKind, Command, Diagnostic, Hover, languages, MarkdownString, Position, ProviderResult, QuickPickItem, Range, Selection, TextDocument, TextEditor, TextEditorRevealType, Uri, window, workspace, WorkspaceEdit} from "vscode";
+import { CancellationToken, CodeAction, CodeActionContext, CodeActionKind, Command, Diagnostic, Hover, languages, MarkdownString, Position, ProviderResult, QuickPickItem, Range, Selection, TextDocument, TextEditor, TextEditorRevealType, Uri, window, workspace, WorkspaceEdit } from "vscode";
 import * as vscode from "vscode";
 import { registerCommand } from "./extension";
-import { executeJavaLanguageServerCommand, getJavaExtension } from "./jdtls/commands";
+import { executeJavaLanguageServerCommand, getJavaExtension, isJavaExtActivated } from "./jdtls/commands";
 import { applyWorkspaceEdit } from "./utils/editUtils";
 
 // Please refer to https://help.eclipse.org/2019-06/index.jsp?topic=%2Forg.eclipse.jdt.doc.isv%2Freference%2Fapi%2Fconstant-values.html
 const UNDEFINED_TYPE: string = "16777218";
 const UNDEFINED_NAME: string = "570425394";
 const UNRESOLVED_CODE: string[] = [UNDEFINED_TYPE, UNDEFINED_NAME];
+const COMMAND_SEARCH_ARTIFACT: string = "maven.artifactSearch";
+const TITLE_RESOLVE_UNKNOWN_TYPE: string = "Resolve unknown type";
 
-// tslint:disable-next-line: export-name
 export function registerArtifactSearcher(context: vscode.ExtensionContext): void {
-    const javaExt: vscode.Extension<any>|undefined = getJavaExtension();
+    const javaExt: vscode.Extension<any> | undefined = getJavaExtension();
     if (!!javaExt) {
-        javaExt.activate().then(async () => {
-            registerCommand(context, "maven.artifactSearch", async (param: any) => {
-                const pickItem: QuickPickItem|undefined = await window.showQuickPick(getArtifactsPickItems(param.className), {placeHolder: "Select the artifact you want to add"});
-                if (pickItem === undefined) {
-                    return;
-                }
-                const edits: WorkspaceEdit[] = await getWorkSpaceEdits(pickItem, param);
-                await applyEdits(Uri.parse(param.uri), edits);
-            });
-            languages.registerHoverProvider("java", {
-                provideHover(document: TextDocument, position: Position, _token: CancellationToken): ProviderResult<Hover> {
-                    return getArtifactsHover(document, position);
-                }
-            });
-            languages.registerCodeActionsProvider("java", {
-                // tslint:disable-next-line: no-shadowed-variable
-                provideCodeActions(document: TextDocument, range: Range | Selection, context: CodeActionContext, _token: CancellationToken): ProviderResult<(Command | CodeAction)[]> {
-                    return getArtifactsCodeActions(document, context, range);
-                }
-            });
-            await executeJavaLanguageServerCommand("java.maven.initializeSearcher", path.join(context.extensionPath, "resources", "IndexData"));
+        const resolver: TypeResolver = new TypeResolver(path.join(context.extensionPath, "resources", "IndexData"));
+
+        registerCommand(context, COMMAND_SEARCH_ARTIFACT, async (param: any) => await resolver.pickAndAddDependency(param));
+
+        languages.registerHoverProvider("java", {
+            provideHover(document: TextDocument, position: Position, _token: CancellationToken): ProviderResult<Hover> {
+                return resolver.getArtifactsHover(document, position);
+            }
+        });
+
+        languages.registerCodeActionsProvider("java", {
+            provideCodeActions(document: TextDocument, range: Range | Selection, codeActionContext: CodeActionContext, _token: CancellationToken): ProviderResult<(Command | CodeAction)[]> {
+                return resolver.getArtifactsCodeActions(document, codeActionContext, range);
+            }
         });
     }
 }
 
-async function getArtifactsPickItems(className: string):  Promise<QuickPickItem[]> {
+class TypeResolver {
+    private dataPath: string;
+    private initialized: boolean = false;
+
+    constructor(dataPath: string) {
+        this.dataPath = dataPath;
+    }
+
+    public async initialize(): Promise<void> {
+        if (!this.initialized) {
+            try {
+                await executeJavaLanguageServerCommand("java.maven.initializeSearcher", this.dataPath);
+                this.initialized = true;
+            } catch (error) {
+                // ignore
+            }
+        }
+    }
+
+    public getArtifactsHover(document: TextDocument, position: Position): Hover | undefined {
+        if (!isJavaExtActivated()) {
+            return undefined;
+        }
+
+        if (!this.initialized) {
+            this.initialize();
+            return undefined;
+        }
+
+        const diagnostics: Diagnostic[] = languages.getDiagnostics(document.uri).filter(value => {
+            return value.code === UNDEFINED_TYPE && position.isAfterOrEqual(value.range.start) && position.isBeforeOrEqual(value.range.end);
+        });
+        if (diagnostics.length !== 0) {
+            const line: number = diagnostics[0].range.start.line;
+            const character: number = diagnostics[0].range.start.character;
+            const className: string = document.getText(diagnostics[0].range);
+            const length: number = document.offsetAt(diagnostics[0].range.end) - document.offsetAt(diagnostics[0].range.start);
+            const param: any = {
+                className,
+                uri: document.uri.toString(),
+                line,
+                character,
+                length
+            };
+            const commandName: string = TITLE_RESOLVE_UNKNOWN_TYPE;
+            const message: string = `\uD83D\uDC49 [${commandName}](command:${COMMAND_SEARCH_ARTIFACT}?${encodeURIComponent(JSON.stringify(param))} "${commandName}")`;
+            const hoverMessage: MarkdownString = new MarkdownString(message);
+            hoverMessage.isTrusted = true;
+            return new Hover(hoverMessage);
+        } else {
+            return undefined;
+        }
+    }
+
+    public getArtifactsCodeActions(document: TextDocument, context: CodeActionContext, _selectRange: Range): CodeAction[] | undefined {
+        if (!isJavaExtActivated()) {
+            return undefined;
+        }
+
+        if (!this.initialized) {
+            this.initialize();
+            return undefined;
+        }
+
+        const diagnostics: Diagnostic[] = context.diagnostics.filter(value => {
+            return UNRESOLVED_CODE.indexOf(String(value.code)) !== -1;
+        });
+        if (diagnostics.length === 1) {
+            const range: Range = diagnostics[0].range;
+            const className: string = document.getText(range);
+            const uri: string = document.uri.toString();
+            const line: number = range.start.line;
+            const character: number = range.start.character;
+            const length: number = document.offsetAt(range.end) - document.offsetAt(range.start);
+            const command: Command = {
+                title: TITLE_RESOLVE_UNKNOWN_TYPE,
+                command: COMMAND_SEARCH_ARTIFACT,
+                arguments: [{
+                    className,
+                    uri,
+                    line,
+                    character,
+                    length
+                }]
+            };
+            const codeAction: CodeAction = {
+                title: `${TITLE_RESOLVE_UNKNOWN_TYPE} '${className}'`,
+                command: command,
+                kind: CodeActionKind.QuickFix
+            };
+            return [codeAction];
+        } else {
+            return [];
+        }
+    }
+
+    public async pickAndAddDependency(param: any): Promise<void> {
+        if (!isJavaExtActivated()) {
+            return;
+        }
+
+        if (!this.initialized) {
+            this.initialize();
+            return;
+        }
+
+        const pickItem: QuickPickItem | undefined = await window.showQuickPick(getArtifactsPickItems(param.className), { placeHolder: "Select the artifact you want to add" });
+        if (pickItem === undefined) {
+            return;
+        }
+        const edits: WorkspaceEdit[] = await getWorkSpaceEdits(pickItem, param);
+        await applyEdits(Uri.parse(param.uri), edits);
+    }
+}
+
+async function getArtifactsPickItems(className: string): Promise<QuickPickItem[]> {
     const searchParam: ISearchArtifactParam = {
         searchType: SearchType.className,
         className: className
@@ -72,10 +181,6 @@ async function getArtifactsPickItems(className: string):  Promise<QuickPickItem[
     return picks;
 }
 
-async function getWorkSpaceEdits(pickItem: QuickPickItem, param: any): Promise<WorkspaceEdit[]> {
-    return await executeJavaLanguageServerCommand("java.maven.addDependency", pickItem.description, pickItem.detail, param.uri, param.line, param.character, param.length);
-}
-
 async function applyEdits(uri: Uri, edits: any): Promise<void> {
     // if the pom is invalid, no change occurs in edits[2]
     if (Object.keys(edits[2].changes).length > 0) {
@@ -97,71 +202,15 @@ async function applyEdits(uri: Uri, edits: any): Promise<void> {
         // tslint:disable-next-line: restrict-plus-operands
         const startLine: number = edits[2].changes[Object.keys(edits[2].changes)[0]][0].range.start.line + LINE_OFFSET; // skip blank line
         const lineNumber: number = edits[2].changes[Object.keys(edits[2].changes)[0]][0].newText.indexOf("<dependencies>") === -1 ? 5 : 7;
-        const editor: TextEditor = await window.showTextDocument(document, {selection: new Range(startLine, 0, startLine + lineNumber, 0), preview: false});
+        const editor: TextEditor = await window.showTextDocument(document, { selection: new Range(startLine, 0, startLine + lineNumber, 0), preview: false });
         editor.revealRange(new Range(startLine, 0, startLine + lineNumber, 0), TextEditorRevealType.InCenter);
     } else {
         window.showInformationMessage("Sorry, the pom.xml file is inexistent or invalid.");
     }
 }
 
-function getArtifactsHover(document: TextDocument, position: Position): Hover|undefined {
-    const diagnostics: Diagnostic[] = languages.getDiagnostics(document.uri).filter(value => {
-        return value.code === UNDEFINED_TYPE && position.isAfterOrEqual(value.range.start) && position.isBeforeOrEqual(value.range.end);
-    });
-    if (diagnostics.length !== 0) {
-        const line: number = diagnostics[0].range.start.line;
-        const character: number = diagnostics[0].range.start.character;
-        const className: string = document.getText(diagnostics[0].range);
-        const length: number = document.offsetAt(diagnostics[0].range.end) - document.offsetAt(diagnostics[0].range.start);
-        const param: any = {
-            className,
-            uri: document.uri.toString(),
-            line,
-            character,
-            length
-        };
-        const commandName: string = "Resolve unknown type";
-        const command: string = "maven.artifactSearch";
-        const message: string = `\uD83D\uDC49 [${commandName}](command:${command}?${encodeURIComponent(JSON.stringify(param))} "${commandName}")`;
-        const hoverMessage: MarkdownString = new MarkdownString(message);
-        hoverMessage.isTrusted = true;
-        return new Hover(hoverMessage);
-    } else {
-        return undefined;
-    }
-}
-
-function getArtifactsCodeActions(document: TextDocument, context: CodeActionContext, _selectRange: Range): CodeAction[] {
-    const diagnostics: Diagnostic[] = context.diagnostics.filter(value => {
-        return UNRESOLVED_CODE.indexOf(String(value.code)) !== -1;
-    });
-    if (diagnostics.length === 1) {
-        const range: Range = diagnostics[0].range;
-        const className: string = document.getText(range);
-        const uri: string = document.uri.toString();
-        const line: number = range.start.line;
-        const character: number = range.start.character;
-        const length: number = document.offsetAt(range.end) - document.offsetAt(range.start);
-        const command: Command = {
-            title: "Resolve unknown type",
-            command: "maven.artifactSearch",
-            arguments: [{
-                className,
-                uri,
-                line,
-                character,
-                length
-            }]
-        };
-        const codeAction: CodeAction = {
-            title: `Resolve unknown type '${className}'`,
-            command: command,
-            kind: CodeActionKind.QuickFix
-        };
-        return [codeAction];
-    } else {
-        return [];
-    }
+async function getWorkSpaceEdits(pickItem: QuickPickItem, param: any): Promise<WorkspaceEdit[]> {
+    return await executeJavaLanguageServerCommand("java.maven.addDependency", pickItem.description, pickItem.detail, param.uri, param.line, param.character, param.length);
 }
 
 export interface IArtifactSearchResult {
