3731b2c6d397c6a0ff95b588f84419fd5fe90959
Zhuo Ning <943297456@users.noreply.github.com>
2020-07-15 17:07:42 +0800

    Fix error: When a Non-Maven terminal is closed, all Maven terminals are shut down. (#513)
    * Fix error: When a Non-Maven terminal is closed, all Maven terminals are shut down.
    
    * Judge before dispose when a terminal is manually closed
    
    * fix tslint error

--- src/mavenTerminal.ts
+++ src/mavenTerminal.ts
@@ -76,15 +76,24 @@ class MavenTerminal implements vscode.Disposable {
     }
 
     public dispose(terminalName?: string): void {
-        if (terminalName && this.terminals[terminalName] !== undefined) {
-            this.terminals[terminalName].dispose();
-            delete this.terminals[terminalName];
-        } else {
+        if (terminalName === undefined) {// If the name is not passed, dispose all.
             Object.keys(this.terminals).forEach((id: string) => {
                 this.terminals[id].dispose();
                 delete this.terminals[id];
             });
+        } else if (this.terminals[terminalName] !== undefined) {
+            this.terminals[terminalName].dispose();
+            delete this.terminals[terminalName];
         }
+    }
+
+    public find(terminal: vscode.Terminal): string | undefined {
+        for (const name in this.terminals) {
+            if (this.terminals[name] === terminal) {
+                return name;
+            }
+        }
+        return;
     }
 }
 
