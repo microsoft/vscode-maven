eba6d365fbf9bd75143e23c3609302190c0e4e4d
Melody618 <Melody618@sjtu.edu.cn>
2021-07-14 16:25:25 +0800

    add inline command for Dependencies node: add a dependency

--- src/explorer/model/DependenciesMenu.ts
+++ src/explorer/model/DependenciesMenu.ts
@@ -14,6 +14,10 @@ export class DependenciesMenu extends Menu implements ITreeItem {
         this.name = "Dependencies";
     }
 
+    public getContextValue(): string {
+        return "DependenciesMenu";
+    }
+
     public async getChildren() : Promise<Dependency[]> {
         const treeNodes = await parseRawDependencyDataHandler(this.project);
         return Promise.resolve(treeNodes);
