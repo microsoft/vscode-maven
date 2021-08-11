720eb61c4faeca5b6008f4e4ac2de8517cea781e
Melody618 <Melody618@sjtu.edu.cn>
2021-08-11 14:36:24 +0800

    only conflict dependencies has set version command

--- src/explorer/model/Dependency.ts
+++ src/explorer/model/Dependency.ts
@@ -29,8 +29,11 @@ export class Dependency extends TreeNode implements ITreeItem {
 
     public getContextValue(): string {
         const root = <Dependency> this.root;
-        if(root.fullArtifactName === this.fullArtifactName) {
+        if (root.fullArtifactName === this.fullArtifactName) {
             return "rootDependency";
+        }
+        if (this.omittedStatus?.status === "conflict") {
+            return "conflictDependency";
         }
         return "Dependency";
     }
