d12a21229789ccb66c0f50f838e35ce8844204c2
Melody618 <Melody618@sjtu.edu.cn>
2021-08-11 13:40:18 +0800

    fix: do not show exclude command on root dependencies

--- src/explorer/model/Dependency.ts
+++ src/explorer/model/Dependency.ts
@@ -28,6 +28,10 @@ export class Dependency extends TreeNode implements ITreeItem {
     }
 
     public getContextValue(): string {
+        const root = <Dependency> this.root;
+        if(root.fullArtifactName === this.fullArtifactName) {
+            return "rootDependency";
+        }
         return "Dependency";
     }
 
