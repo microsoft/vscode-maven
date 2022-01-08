8a454452be1fdf918a3cd4f0edf634b53a1691ef
emliunix <emliunix@hotmail.com>
2022-01-08 16:55:03 +0800

    Handle settings.xml path quoting

--- src/utils/mavenUtils.ts
+++ src/utils/mavenUtils.ts
@@ -67,7 +67,7 @@ async function executeInBackground(mvnArgs: string, pomfile?: string): Promise<a
     const cwd: string | undefined = workspaceFolder ? path.resolve(workspaceFolder.uri.fsPath, mvn, "..") : undefined;
     const userArgs: string | undefined = Settings.Executable.options(pomfile);
     const mvnSettingsFile: string | undefined = Settings.getSettingsFilePath();
-    const mvnSettingsArg: string | undefined = mvnSettingsFile ? `-s ${mvnSettingsFile}` : undefined;
+    const mvnSettingsArg: string | undefined = mvnSettingsFile ? `-s "${await mavenTerminal.formattedPathForTerminal(mvnSettingsFile)}"` : undefined;
     const matched: RegExpMatchArray | null = [mvnSettingsArg, mvnArgs, userArgs].filter(Boolean).join(" ").match(/(?:[^\s"]+|"[^"]*")+/g); // Split by space, but ignore spaces in quotes
     const args: string[] = matched !== null ? matched : [];
     if (pomfile) {
@@ -128,7 +128,7 @@ export async function executeInTerminal(options: {
     const mvnSettingsFile: string | undefined = Settings.getSettingsFilePath();
     const fullCommand: string = [
         mvnString,
-        mvnSettingsFile && `-s ${await mavenTerminal.formattedPathForTerminal(mvnSettingsFile)}`,
+        mvnSettingsFile && `-s "${await mavenTerminal.formattedPathForTerminal(mvnSettingsFile)}"`,
         command.trim(),
         pomfile && `-f "${await mavenTerminal.formattedPathForTerminal(pomfile)}"`,
         Settings.Executable.options(pomfile)
