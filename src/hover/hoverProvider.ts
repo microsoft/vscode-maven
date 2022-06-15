1cd268d6c06d661e24c92c6504c15a3ea6c806e3
Yan Zhang <yanzh@microsoft.com>
2022-06-15 22:25:48 +0800

    adopt htmlparser2 in hover provider
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/hover/hoverProvider.ts
+++ src/hover/hoverProvider.ts
@@ -1,29 +1,45 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import { Element, isTag, Node } from "domhandler";
 import * as _ from "lodash";
 import * as vscode from "vscode";
 
 import { mavenExplorerProvider } from "../explorer/mavenExplorerProvider";
 import { MavenProject } from "../explorer/model/MavenProject";
-import { ElementNode, getCurrentNode, XmlTagName } from "../utils/lexerUtils";
+import { ElementNode, getCurrentNode, getTextFromNode, XmlTagName } from "../utils/lexerUtils";
 
 class HoverProvider implements vscode.HoverProvider {
     public async provideHover(document: vscode.TextDocument, position: vscode.Position, _token: vscode.CancellationToken): Promise<vscode.Hover | undefined> {
         const currentNode: ElementNode | undefined = getCurrentNode(document.getText(), document.offsetAt(position));
-        if (currentNode === undefined || currentNode.contentStart === undefined || currentNode.contentEnd === undefined) {
+        if (currentNode === undefined || currentNode.startIndex === null || currentNode.endIndex === null) {
             return undefined;
         }
-        const targetRange: vscode.Range = new vscode.Range(document.positionAt(currentNode.contentStart), document.positionAt(currentNode.contentEnd));
-        switch (currentNode.tag) {
+
+        let tagNode;
+        if (isTag(currentNode)) {
+            tagNode = currentNode;
+        } else if (currentNode.parent && isTag(currentNode.parent)) {
+            tagNode = currentNode.parent;
+        } else {
+            // TODO: should we recursively traverse up to find nearest tag node?
+        }
+
+        switch (tagNode?.tagName) {
             case XmlTagName.GroupId:
             case XmlTagName.ArtifactId:
             case XmlTagName.Version: {
-                const siblingNodes: ElementNode[] = _.get(currentNode, "parent.children", []);
-                const artifactIdNode: ElementNode | undefined = siblingNodes.find(elem => elem.tag === XmlTagName.ArtifactId);
-                const groupIdNode: ElementNode | undefined = siblingNodes.find(elem => elem.tag === XmlTagName.GroupId);
-                const groupIdHint: string | undefined = groupIdNode && groupIdNode.text;
-                const artifactIdHint: string | undefined = artifactIdNode && artifactIdNode.text;
+                const targetNode = tagNode.parent;
+                const targetRange: vscode.Range = new vscode.Range(
+                    targetNode && targetNode.startIndex !== null ? document.positionAt(targetNode?.startIndex) : position,
+                    targetNode && targetNode.endIndex !== null ? document.positionAt(targetNode?.endIndex) : position,
+                );
+
+                const siblingNodes: Node[] = tagNode.parent?.children ?? [];
+                const artifactIdNode: Element | undefined = siblingNodes.find(elem => isTag(elem) && elem.tagName === XmlTagName.ArtifactId) as Element | undefined;
+                const groupIdNode: Element | undefined = siblingNodes.find(elem => isTag(elem) && elem.tagName === XmlTagName.GroupId) as Element | undefined;
+                const groupIdHint = getTextFromNode(groupIdNode?.firstChild);
+                const artifactIdHint = getTextFromNode(artifactIdNode?.firstChild);
                 if (groupIdHint && artifactIdHint) {
                     const mavenProject: MavenProject | undefined = mavenExplorerProvider.getMavenProject(document.uri.fsPath);
                     if (!mavenProject) {
