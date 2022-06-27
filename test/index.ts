55b0cb6f11980e547a24032250c170717c240f84
Yan Zhang <yanzh@microsoft.com>
2022-06-27 14:08:05 +0800

    follow @vscode/test-electron guide
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- test/index.ts
+++ test/index.ts
@@ -1,37 +1,19 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTests } from "@vscode/test-electron";
 import * as cp from "child_process";
 import * as os from "os";
 import * as path from "path";
-import { downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath, runTests } from "vscode-test";
 
 async function main(): Promise<void> {
     try {
         const vscodeExecutablePath = await downloadAndUnzipVSCode();
-        // Platform-specific when using @vscode/test-electron
-        // let platform;
-        // switch (os.platform()) {
-        //     case "linux":
-        //         platform = "linux-x64";
-        //         break;
-        //     case "win32":
-        //         platform = "win32-x64-archive";
-        //         break;
-        //     case "darwin":
-        //         platform = os.arch() === "arm64" ? "darwin-arm64" : "darwin"
-        //         break;
-        //     default:
-        //         console.error(`unsupported platform: ${os.platform()}`);
-        //         return;
-        // }
-        // const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath, platform);
-        const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
 
-        // Resolve extension dependencies
-        cp.spawnSync(cliPath, ["--install-extension", "redhat.java"], {
-            encoding: "utf-8",
-            stdio: "inherit",
+        const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+        cp.spawnSync(cli, [...args, '--install-extension', 'redhat.java'], {
+            encoding: 'utf-8',
+            stdio: 'inherit'
         });
 
         // The folder containing the Extension Manifest package.json
