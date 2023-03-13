9cbc25fd757afca4f260ece7508b0ac5d0177ca4
Yan Zhang <Eskibear@users.noreply.github.com>
2023-03-13 14:20:14 +0800

    refactor: add type ProfilesMenu (#950)

--- src/explorer/model/Menu.ts
+++ src/explorer/model/Menu.ts
@@ -10,10 +10,6 @@ const CONTEXT_VALUE: string = "maven:menu";
 export abstract class Menu implements ITreeItem {
     protected name: string;
 
-    constructor(
-        public project: MavenProject,
-    ) { }
-
     public abstract getChildren(): ITreeItem[] | undefined | Promise<ITreeItem[] | undefined>;
 
     public getContextValue(): string {
@@ -24,5 +20,13 @@ export abstract class Menu implements ITreeItem {
         const treeItem: vscode.TreeItem = new vscode.TreeItem(this.name, vscode.TreeItemCollapsibleState.Collapsed);
         treeItem.iconPath = new vscode.ThemeIcon("folder");
         return treeItem;
+    }
+}
+
+export abstract class ProjectMenu extends Menu {
+    constructor(
+        public project: MavenProject,
+    ) {
+        super();
     }
 }
