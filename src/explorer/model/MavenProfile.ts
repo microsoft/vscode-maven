01897af6592c8e6f9833398dbec8d56bea6f404a
Yan Zhang <yanzh@microsoft.com>
2023-03-16 15:21:09 +0800

    address comments
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/explorer/model/MavenProfile.ts
+++ src/explorer/model/MavenProfile.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import * as vscode from "vscode";
 import { ITreeItem } from "./ITreeItem";
 import { MavenProject } from "./MavenProject";
